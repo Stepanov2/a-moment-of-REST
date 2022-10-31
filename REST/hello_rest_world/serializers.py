14f7f4e0e2765b82c48e3f18fbf98011160268ae
Dmitry Stepanov <bugagolick@gmail.com>
2022-10-31 14:36:20 +0300

    Basic Serializers, api-viewsets

--- REST/hello_rest_world/serializers.py
+++ REST/hello_rest_world/serializers.py
@@ -1,2 +1,15 @@
 from .models import *
 from rest_framework import serializers
+
+
+class DancerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Dancer
+        fields = ['pk', 'name', 'born', 'dance_groups']
+
+
+class DanceGroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = DanceGroup
+        fields = ['pk', 'name', 'dancer_set']
+
--- REST/hello_rest_world/views.py
+++ REST/hello_rest_world/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from rest_framework import permissions
+from .models import Dancer, DanceGroup, DanceGroupDancers
+from .serializers import *
 # Create your views here.
+class DancerViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Dancer.objects.all().order_by('name')
+    serializer_class = DancerSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class DanceGroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = DanceGroup.objects.all()
+    serializer_class = DanceGroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
