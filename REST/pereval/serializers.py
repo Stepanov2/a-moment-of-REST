90816238a1b07c6a570a26216ae53568adfa56f6
Dmitry Stepanov <bugagolick@gmail.com>
2022-11-11 14:32:17 +0300

    Children serializers started
    source='*' my ass! Shouldn't this be the default!?

--- REST/pereval/serializers.py
+++ REST/pereval/serializers.py
@@ -18,11 +18,24 @@ class ImagesSerializer(serializers.HyperlinkedModelSerializer):
     def get_data(self, obj):
         """Returns Image contents in JSON format"""
         #todo
-        return '<image will be here>'
+        return f'<image "{obj.title}" will be here>'
+
+
+class CoordsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Added
+        fields = ['latitude', 'longitude', 'height']
+
+
+class LevelSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Added
+        fields = ['level_winter', 'level_summer', 'level_autumn', 'level_spring']
+
 
 class PerevalSerializer(serializers.HyperlinkedModelSerializer):
-    coords = serializers.SerializerMethodField(read_only=True)
-    level = serializers.SerializerMethodField(read_only=True)
+    coords = CoordsSerializer(source='*')
+    level = LevelSerializer(source='*')
     user = UserSerializer()
     images = ImagesSerializer(source='image_set', many=True)
 
@@ -30,34 +43,4 @@ class PerevalSerializer(serializers.HyperlinkedModelSerializer):
         model = Added
         fields = ['pk', 'beauty_title', 'title', 'other_titles', 'connect', 'add_time', 'user',
                   'coords', 'level', 'images']
-
-    def get_coords(self, obj):
-        # print(obj.level_autumn.__dict__)
-        return {
-            'latitude': obj.latitude,
-            'longitude': obj.longitude,
-            'height': obj.height,
-        }
-
-    def get_level(self, obj):
-        return {}
-
-
-
-# class DancerSerializer(serializers.HyperlinkedModelSerializer):
-#     num_groups = serializers.SerializerMethodField(read_only=True)
-#
-#     class Meta:
-#         model = Dancer
-#         fields = ['pk', 'name', 'born', 'dance_groups', 'age', 'num_groups']
-#
-#
-#     def get_num_groups(self, obj):
-#         return obj.dance_groups.count()
-#
-#
-# class DanceGroupSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = DanceGroup
-#         fields = ['pk', 'name', 'dancer_set']
 
